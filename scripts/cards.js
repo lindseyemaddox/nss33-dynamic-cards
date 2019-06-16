8b98dd82035f6be2f7216ffc852bee95892dbf65
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-06-16 15:12:50 -0500

    creating element with unique identifier for fucks sake

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,60 +1,32 @@
 
 // When the user enters in text into the text area and then clicks the create button,
 // use a factory function that creates a new DOM component
-// that has a border,
-// and includes it's own delete button.
-
+// that has a border, and includes it's own delete button.
 // Insert that new component into the DOM.
 
 // When the user clicks the Delete button, the containing card, and no other cards,
 // should then be removed from the DOM. Not just made invisible, actually removed from the DOM. (remove child)
 
+const container = document.querySelector("#textarea");
 const createButton = document.querySelector("#create-btn");
-let container = document.querySelector("#main");
-const lotsOfCreateButtons = document.querySelectorAll(".create-btn");
+let unique = 0;
 
 if (container !== "") {
-  createButton.addEventListener("click", (index) => {
-
-    addComponentToDom(createComponent(index));
-
-    let deleteButton = document.querySelectorAll(".deleteBtn");
-
-    for(let i = 0; i < deleteButton.length; i++) {
-      deleteButton.addEventListener("click", function (i) {
-
-
-          let buttonId = event.target.id;
-          let idArray = buttonId.split("-");
-          let idNumber = idArray[1];
-          let parkId = "deleteBtn-" + idNumber;
-          let parkName = document.getElementById(parkId).textContent;
-          deleteComponentFromDom(deleteSearchComponent(parkName));
-
-      });
-    }
-  }
-  );
+  createButton.addEventListener("click", () => {
+    unique++;
+    addComponentToDom(createComponent(unique));
+  });
 }
 
-
-function addComponentToDom(index) {
-  document.getElementById("results").innerHTML += index;
+function addComponentToDom(poop) {
+  document.getElementById("results").innerHTML += poop;
 }
 
-function createComponent(index) {
+function createComponent(unique) {
   return `
+  <div class="border" id="deleteCase-${unique}">
     <h3>Border Element</h3>
-    <button class="deleteBtn" id="deleteBtn-${index}">Delete</button>
+    <button class="delete-btn" id="deleteBtn-${unique}">Delete</button>
+  </div>
   `;
 }
-
-function deleteComponentFromDom(parkComponent) {
-  document.getElementById("results").innerHTML += parkComponent;
-}
-
-function deleteSearchComponent() {
-  return `
-  `;
-}
-
