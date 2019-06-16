c36f09cb5993f56b045ac563dde113fadf01482d
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-06-15 21:52:12 -0500

    eip tryimh ho hry iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiic clear wq q :q
    :wq

--- scripts/cards.js
+++ scripts/cards.js
@@ -3,38 +3,38 @@
 // use a factory function that creates a new DOM component
 // that has a border,
 // and includes it's own delete button.
-
 // Insert that new component into the DOM.
 
 // When the user clicks the Delete button, the containing card, and no other cards,
 // should then be removed from the DOM. Not just made invisible, actually removed from the DOM. (remove child)
 
+const container = document.querySelector("#textarea");
 const createButton = document.querySelector("#create-btn");
-let container = document.querySelector("#main");
-const lotsOfCreateButtons = document.querySelectorAll(".create-btn");
 
 if (container !== "") {
   createButton.addEventListener("click", (index) => {
-
     addComponentToDom(createComponent(index));
 
-    let deleteButton = document.querySelectorAll(".deleteBtn");
+      let deleteButton = document.querySelectorAll(".deleteBtn");
 
-    for(let i = 0; i < deleteButton.length; i++) {
-      deleteButton.addEventListener("click", i => () {
+      for(let i=0; i < deleteButton.length; i++) {
+        deleteComponentFromDom(deleteSearchComponent(index[i], i));
+      }
 
+      deleteButton.forEach(button => {
+        button.addEventListener("click", function (i) {
           let buttonId = event.target.id;
           let idArray = buttonId.split("-");
           let idNumber = idArray[1];
-          let parkId = "deleteBtn-" + idNumber;
-          let parkName = document.getElementById(parkId).textContent;
-          deleteComponentFromDom(deleteSearchComponent(parkName));
-
+          let deleteId = "deleteBtn-" + idNumber;
+          let deleteBtn = document.getElementById(deleteId).textContent;
+          deleteComponentFromDom(deleteSearchComponent(deleteBtn));
+        });
       });
-    }
-  }
-  );
+    });
 }
+
+
 
 
 function addComponentToDom(index) {
@@ -43,17 +43,19 @@ function addComponentToDom(index) {
 
 function createComponent(index) {
   return `
+  <div class="border" id="deleteCase-${index}">
     <h3>Border Element</h3>
     <button class="deleteBtn" id="deleteBtn-${index}">Delete</button>
+  </div>
   `;
 }
 
-function deleteComponentFromDom(parkComponent) {
-  document.getElementById("results").innerHTML += parkComponent;
+function deleteComponentFromDom(i) {
+  document.getElementById("results").innerHTML += i;
 }
 
-function deleteSearchComponent() {
+function deleteSearchComponent(child) {
   return `
+    ${child}
   `;
 }
-
